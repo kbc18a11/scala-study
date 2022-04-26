659e85f90b59f85b0ec30649c9764291fc0d6704
kbc18a11 <wada121400@gmail.com>
2022-04-26 23:04:11 +0900

    次 https://scala-text.github.io/scala_text/control-syntax.html#while%E5%BC%8F

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,5 +1,12 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val age: Int = 5
+    val isSchoolStarted: Boolean = false
+
+    if (1 <= age && age <= 6 && !isSchoolStarted) {
+      return println("幼児です")
+    }
+
+    println("幼児ではありません")
   }
 }
