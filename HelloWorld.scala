dd6b7ccf6ae55915edb3da4503ea9637cd18fb32
kbc18a11 <wada121400@gmail.com>
2022-04-29 17:40:58 +0900

    次　https://scala-text.github.io/scala_text/object.html

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,12 +1,29 @@
+class Point(val x: Int, val y: Int) {
+  def +(p: Point): Point = {
+    new Point(x + p.x, y + p.y)
+  }
+  override def toString(): String = "(" + x + ", " + y + ")"
+}
+
+class Adder {
+  def add(x: Int)(y: Int): Int = x + y
+}
+
+class Point3D(val x: Int, val y: Int, val z: Int) {}
+
 object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    val age: Int = 5
-    val isSchoolStarted: Boolean = false
-
-    if (1 <= age && age <= 6 && !isSchoolStarted) {
-      return println("幼児です")
+  def loopFrom0To9(): Unit = {
+    var i = 0
+    while (i < 10) {
+      println(i)
+      i += 1
     }
+  }
 
-    println("幼児ではありません")
+  def main(args: Array[String]): Unit = {
+    val p = new Point3D(10, 20, 30)
+    println(p.x) // 10
+    println(p.y) // 20
+    println(p.z) // 30
   }
 }
